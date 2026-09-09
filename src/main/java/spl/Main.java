5cbcda6a95b9c8ab427f714d263154d3605a7e8b
Keegan-l-star <u24594522@tuks.co.za>
2026-09-10 00:17:02 +0200

    The tree is done and works with an example of how it is used.

--- src/main/java/spl/Main.java
+++ src/main/java/spl/Main.java
@@ -1,7 +1,26 @@
 package spl;
 
+/*
+This was jsut for a test of nodes
+import spl.tree.RootNode;
+import spl.tree.InnerNode;
+import spl.tree.TerminalNode;
+import spl.tree.TreeFactory;
+*/
 public class Main {
     public static void main(String[] args) {
         System.out.println("SPL Compiler");
+        /*
+           TreeFactory factory = new TreeFactory();
+           RootNode root = factory.getTree();
+           InnerNode branch = factory.getInner("branch-1");
+           TerminalNode leaf1 = factory.getTerminal("leaf-1");
+           TerminalNode leaf2 = factory.getTerminal("leaf-2");
+           root.addChild(branch);
+           branch.addChild(leaf1);
+           branch.addChild(leaf2);
+           
+           System.out.println("Root content: " + root.getContent());
+        */
     }
 }
